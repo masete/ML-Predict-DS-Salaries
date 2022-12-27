7e26ac88b803faab80041bbf825f660f4c3a4d64
masete <nicholasmasete72@gmail.com>
2022-12-27 10:42:53 +0300

    change in port number

--- FLASKAPI/requests.py
+++ FLASKAPI/requests.py
@@ -1,7 +1,7 @@
 import requests 
 from data_input import data_in
 
-URL = 'http://127.0.0.1:5000/predict'
+URL = 'http://127.0.0.1:8080/predict'
 headers = {"Content-Type": "application/json"}
 data = {"input": data_in}
 
